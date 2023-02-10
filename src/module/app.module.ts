8b2d0718d1fc4e615ca6ae694443fc90009d5e09
ncfakude30 <ncfakude30@gmail.com>
2023-02-10 10:33:08 +0200

    Updated AppModule to use .env file
    Updated AppModule to use the dotenv file when retrieving database credentials.

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -3,17 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlaylistModule } from './playlist.module';
 import { TrackModule } from './track.module';
 import { AuthMiddleware } from 'src/security/auth.middleware';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Melvinstumbo@321',
-      database: 'music_db',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
